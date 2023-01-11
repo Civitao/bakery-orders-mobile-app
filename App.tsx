122a7e52669f0d409f79ea9815455a70412b6fd7
Vitor Marques <104794326+Civitao@users.noreply.github.com>
2023-01-11 12:56:10 -0300

    organizing indentation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import { useFonts, Inter_300Light, Inter_400Regular, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
 import { StatusBar } from 'react-native';
-
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
-
 import { CartItemsProvider } from './src/contexts/CartItemsState'
-
 import { Routes } from './src/routes';
+
 export default function App() {
+  
   const [fontsLoaded] = useFonts({
     Inter_300Light,
     Inter_400Regular,
@@ -16,21 +15,16 @@ export default function App() {
   });
 
   return (
-      <CartItemsProvider>
+    <CartItemsProvider>
       <Background>
-      <StatusBar barStyle="dark-content"
-      backgroundColor='transparent'
-      translucent />
-      
-      {/* { fontsLoaded } */}
-
-  
-
-       {fontsLoaded ? <Routes /> :  <Loading/>}
-      
-       </Background>
-      </ CartItemsProvider>
-
+        <StatusBar 
+         barStyle="dark-content"
+         backgroundColor='transparent'
+         translucent
+        />
+         {fontsLoaded ? <Routes /> :  <Loading/>}
+      </Background>
+    </ CartItemsProvider>
   );
 }
 
